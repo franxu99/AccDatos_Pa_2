ec1d82e530f774e23b97e6dfc4f0929965d2d5cf
ADM_DAM2B_09 <ADM_DAM2B_09@MASTER_LENOVO16>
2023-09-25 20:20:56 +0200

    Agregar archivos de proyecto.

---
+++ AccDatos_Pa_2/AccesoDatos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace AccDatos_Pa_2
+{
+    internal class AccesoDatos
+    {
+        String myConnectionString = "server=localhost;" +
+            "port=3306;" +
+            "user=root;" +
+            "database=sakila;" +
+            "password=1234;"
+            ;
+        MySqlConnection _conn;
+        MySqlCommand _cmd;
+        public AccesoDatos() {
+            try
+            {
+                _conn = new MySqlConnection(myConnectionString);
+                _conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
