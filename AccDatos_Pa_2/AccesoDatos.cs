97c1d089afdc4341230bf892f6394fa3ea6e0ebb
franxu99 <110120388+franxu99@users.noreply.github.com>
2023-10-02 18:37:05 +0200

    0.3

--- AccDatos_Pa_2/AccesoDatos.cs
+++ AccDatos_Pa_2/AccesoDatos.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using MySql.Data.MySqlClient;
+using System.Windows.Media;
 
 namespace AccDatos_Pa_2
 {
@@ -18,7 +20,8 @@ namespace AccDatos_Pa_2
             ;
         MySqlConnection _conn;
         MySqlCommand _cmd;
-        public AccesoDatos() {
+        public AccesoDatos()
+        {
             try
             {
                 _conn = new MySqlConnection(myConnectionString);
@@ -29,5 +32,69 @@ namespace AccDatos_Pa_2
                 MessageBox.Show(ex.Message);
             }
         }
+
+        public int AltaDePelicula(string title, string description, int release_year, int language_id, int original_lenguage_id, int rental_duration, double rental_rate, int length, double replacement_cost, string rating, string special_features)
+        {
+            int resultado = -98;
+            try
+            {
+                _cmd = new MySqlCommand();
+                _cmd.Connection = _conn;
+                _cmd.CommandType = CommandType.StoredProcedure;
+                _cmd.CommandText = "InsertaAltaPelicula";
+
+                //Parametros Input
+
+                _cmd.Parameters.AddWithValue("_title", title);
+                _cmd.Parameters["_title"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_description", description);
+                _cmd.Parameters["_description"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_release_year", release_year);
+                _cmd.Parameters["_release_year"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_language_id", language_id);
+                _cmd.Parameters["_language_id"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_original_lenguage_id", original_lenguage_id);
+                _cmd.Parameters["_original_lenguage_id"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_rental_duration", rental_duration);
+                _cmd.Parameters["_rental_duration"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_rental_rate", rental_rate);
+                _cmd.Parameters["_rental_rate"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_length", length);
+                _cmd.Parameters["_length"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_replacement_cost", replacement_cost);
+                _cmd.Parameters["_replacement_cost"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_rating", rating);
+                _cmd.Parameters["_rating"].Direction = ParameterDirection.Input;
+
+                _cmd.Parameters.AddWithValue("_special_features", special_features);
+                _cmd.Parameters["_special_features"].Direction = ParameterDirection.Input;
+
+                //Parametros Output
+
+                _cmd.Parameters.Add(new MySqlParameter("_res", MySqlDbType.Int32));
+                _cmd.Parameters["_res"].Direction = ParameterDirection.Output;
+
+                _cmd.ExecuteNonQuery();
+
+                resultado = (int)_cmd.Parameters["_res"].Value;
+                return resultado;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return resultado;
+            }
+        }
+
     }
 }
--- AccDatos_Pa_2/MainWindow.xaml.cs
+++ AccDatos_Pa_2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Org.BouncyCastle.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,29 @@ namespace AccDatos_Pa_2
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int res = -99;
+            AccesoDatos ad = new AccesoDatos();
+            res = ad.AltaDePelicula(tb_title.Text,
+                                    tb_description.Text,
+                                    int.Parse(tb_release_year.Text),
+                                    int.Parse(tb_language_id.Text),
+                                    int.Parse(tb_original_lenguage_id.Text),
+                                    int.Parse(tb_rental_duration.Text),
+                                    double.Parse(tb_rental_rate.Text),
+                                    int.Parse(tb_length.Text),
+                                    double.Parse(tb_replacement_cost.Text),
+                                    tb_rating.Text,
+                                    tb_special_features.Text);
+            MessageBox.Show(res.ToString());
+            if(res == 0)
+            {
+                MessageBox.Show("Pelicula ingresada con exito");
+            }
         }
     }
 }
